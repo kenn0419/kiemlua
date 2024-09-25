12986eab0f2c9408b8f7340a26a62ff1f1589ee0
Quoc Bao <tranbao7008@gmail.com>
2024-09-25 08:54:15 +0700

    third commit

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
-const btnLeft = document.querySelector('.btn-left');
-const btnRight = document.querySelector('.btn-right');
-const arrowUp = document.querySelector('.arrow-up');
-const exitBtn = document.querySelector('#menu .entry .exit');
-const menuModal = document.querySelector('#menu');
-const menuBtn = document.querySelector('.menuBtn');
-
-exitBtn.addEventListener('click', function () {
-    menuModal.style.display = 'none';
-})
-
-menuBtn.addEventListener('click', function (e) {
-    e.stopPropagation();
-    menuModal.style.display = 'block';
-})
-
-document.addEventListener('click', function (e) {
-    if ((!menuModal.contains(e.target) && menuModal.style.display === 'block')) {
-        menuModal.style.display = 'none';
-    }
-})
+// const arrowUp = document.querySelector('.arrow-up');
 
 
-window.addEventListener('scroll', function () {
-    if (window.scrollY >= 1000) {
-        arrowUp.style.display = 'block';
-    } else {
-        arrowUp.style.display = 'none';
-    }
-})
+// window.addEventListener('scroll', function () {
+//     if (window.scrollY >= 1000) {
+//         arrowUp.style.display = 'block';
+//     } else {
+//         arrowUp.style.display = 'none';
+//     }
+// })
 
-arrowUp.addEventListener('click', function () {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-})
+// arrowUp.addEventListener('click', function () {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+// })
 
 document.addEventListener('DOMContentLoaded', function () {
     new Swiper('.mySwiper', {
